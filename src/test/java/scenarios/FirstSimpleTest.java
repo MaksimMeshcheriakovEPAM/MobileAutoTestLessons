05f89336e1eaa16a38651dbf99c1b1de4d1e04c6
Maksim Meshcheriakov <Maksim_Meshcheriakov@epam.com>
2017-12-27 21:00:43 +0300

    Description (in comments) of several lacks of tests

--- src/test/java/scenarios/FirstSimpleTest.java
+++ src/test/java/scenarios/FirstSimpleTest.java
@@ -15,23 +15,25 @@ public class FirstSimpleTest extends Driver {
         //prepareAndroidWeb();
         prepareDriver("Android",
                 "C:\\Users\\Maksim_Meshcheriakov\\IdeaProjects\\MobileAutoTestLessons\\src\\main\\resources",
-                /*"ContactManager.apk"*/ null);
+                // ABSOLUTE path! #facepalm
+                "ContactManager.apk"/* null*/);
 
+    }
+
+    @Test(description = "Open website")
+    public void webTest() throws InterruptedException {
+        driver.get("http://iana.org"); // this always ends OK; it's a drawback.
+        // Other way to check good/bad result has to be implemented.
+        Thread.sleep(5000); // This is a reason to dismissal.
+        System.out.println("Site opening done");
     }
 
     //@Test(description = "This simple test just click on button 'Add contact'")
     public void simplestTest(){
         String app_package_name = "com.example.android.contactmanager:id/";
         By add_btn = By.id(app_package_name + "addContactButton");
-        driver.findElement(add_btn).click();
+        driver.findElement(add_btn).click(); // The result of clicking doesn't checked.
         System.out.println("Simplest Appium test done");
-    }
-
-    @Test(description = "Open website")
-    public void webTest() throws InterruptedException {
-        driver.get("http://iana.org");
-        Thread.sleep(5000);
-        System.out.println("Site opening done");
     }
 
 
